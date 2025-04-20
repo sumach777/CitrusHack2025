5fb01a94589f80ee8f1b56c605efa3b31adf118f
SumedhaCh <sumedha.chalasani@gmail.com>
2025-04-20 05:55:37 -0700

    main allows us to run the fully integrated app

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
-from .helper import helper
+from gui import AudioApp
+from audioprocessing import AudioProcessor
 
 def main():
-    print ("Hello from the main file")
-    helper()
-
-def test():
-    return 0
+    app = AudioApp()
+    root = app.create_window()
+    
+    processor = AudioProcessor(app)
+    app.set_processor(processor)
+    processor.process_audio()
+    
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
